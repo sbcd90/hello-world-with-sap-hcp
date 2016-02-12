8c650d0ea08cbadf1a524f3de6f7e63e351b2894
Subhobrata Dey <sbcd90@gmail.com>
2016-02-11 19:12:30 -0500

    initial commit

---
+++ src/main/java/com/sap/hcp/HelloWorldApplication.java
@@ -0,0 +1,27 @@
+package com.sap.hcp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HelloWorldApplication extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public HelloWorldApplication() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello World");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
